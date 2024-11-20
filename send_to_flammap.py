df644170d6e1ec4d93c19017b5b337caf5af8010
Никита <nikita.ovcherenko777@mail.ru>
2024-11-20 16:02:15 +0500

    send_photo

---
+++ send_to_flammap.py
@@ -0,0 +1,22 @@
+import pyautogui
+import time
+
+# Запустите FlamMap
+import subprocess
+
+flammap_path = r"C:\Workspace\FlamMap6\FlamMap6.exe"
+subprocess.Popen(flammap_path)
+time.sleep(5)  # Подождите, пока программа запустится
+
+
+
+
+pyautogui.doubleClick(x=589, y=388)
+
+time.sleep(1)
+
+file_path = r'Lassen Volcanic National Park.tif'
+pyautogui.write(file_path)
+pyautogui.press('enter')
+
+print(pyautogui.position())
